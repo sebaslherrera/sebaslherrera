eac6cdf2492e14c7666775966fcf5a5f99c44fe4
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-11 15:32:06 -0500

    Sidebar of mobile screens finish with animations

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,9 +4,14 @@ import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const sideBarHandler = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <>
-      <Navbar />
+      <Navbar sideBarHandler={sideBarHandler} />
+      <Sidebar isOpen={isOpen} sideBarHandler={sideBarHandler} />
       {children}
       <Footer />
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,21 @@
-import React from "react";
-import logo from "../assets/logo.svg";
-import { FaAlignRight } from "react-icons/fa";
-import PageLinks from "../constants/links";
-const Navbar = () => {
+import React from "react"
+import logo from "../assets/logo.svg"
+import { FaAlignRight } from "react-icons/fa"
+import PageLinks from "../constants/links"
+const Navbar = ({ sideBarHandler }) => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button type="button" className="toggle-btn">
+          <button type="button" className="toggle-btn" onClick={sideBarHandler}>
             <FaAlignRight></FaAlignRight>
           </button>
         </div>
         <PageLinks styleClass="nav-links"></PageLinks>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
