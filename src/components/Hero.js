0fb758b7197f1f2d6f691dfcccc94d20460d43dc
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-10 21:21:58 -0500

    Added header and hero image TODO: change example photo on hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,45 @@
-import React from "react"
-import Image from "gatsby-image"
-import { Link } from "gatsby"
-import { graphql, useStaticQuery } from "gatsby"
-import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+import React from "react";
+import Image from "gatsby-image";
+import { Link } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
+import SocialLinks from "../constants/socialLinks";
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query);
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I'm Sebastian</h1>
+            <h4>Backend engineer</h4>
+            <Link to="/contact" className="btn">
+              Contact me!
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
