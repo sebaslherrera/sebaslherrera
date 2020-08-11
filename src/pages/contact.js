7c0e9c299f79bd00603184193fa1c1d694c8a7c5
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-11 18:30:43 -0500

    Added contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,40 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>Get in touch</h3>
+          <form action="https://formspree.io/mzbjwvrr" method="POST">
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="name"
+                name="name"
+                className="form-control"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-control"
+              ></textarea>
+              <button type="submit" className="submit-btn btn">
+                Sumbit here
+              </button>
+            </div>
+          </form>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default contact
