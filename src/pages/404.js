2e95f75aba4808c1a4b93b01c15d236cbe16f116
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-11 16:53:53 -0500

    Done custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,60 @@
 import React from "react"
+import { useEffect, useState } from "react"
 import Layout from "../components/Layout"
 import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [item, setItem] = useState("")
+
+  const gifHandler = () => {
+    fetch(
+      "https://api.giphy.com/v1/gifs/random?api_key=jLQ4mbCweCRUhg48LKf6auR6eyTqtK6d&tag=404&rating=g"
+    )
+      .then(res => res.json())
+      .then(
+        result => {
+          const {
+            data: { image_mp4_url: image_mp4_url },
+          } = result
+          setIsLoaded(true)
+          setItem(image_mp4_url)
+        },
+        error => {
+          setIsLoaded(true)
+          setError(error)
+        }
+      )
+  }
+
+  useEffect(() => {
+    gifHandler()
+  }, [])
+
+  return (
+    <Layout>
+      <main className="error-page">
+        <div className="error-container">
+          <h2>
+            You might want to check the url, or{" "}
+            <Link to="/">go back to the homepage</Link>. Or just check out some
+            cool gifs, entirely your choice.
+          </h2>
+          <Link to="/" className="btn">
+            Back Home
+          </Link>
+          {error === null && isLoaded && (
+            <video className="gif" src={item} autoPlay loop></video>
+          )}
+          <button className="btn" onClick={gifHandler}>
+            Generate Another Gif
+          </button>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
