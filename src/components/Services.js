87d9cdd077a64e6d7772a46f1433652c33a7a4c4
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-10 21:32:26 -0500

    Added services section TODO: replace text data to real one

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,25 @@
-import React from "react"
-import Title from "./Title"
-import services from "../constants/services"
+import React from "react";
+import Title from "./Title";
+import services from "../constants/services";
 const Services = () => {
-  return <h2>services component</h2>
-}
+  return (
+    <section className="section bg-gray">
+      <Title title="Services" />
+      <div className="section-center services-center">
+        {services.map((service) => {
+          const { id, icon, title, text } = service;
+          return (
+            <article key={id} className="service">
+              {icon}
+              <h4>{title}</h4>
+              <h4 className="underline"></h4>
+              <p>{text}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Services
+export default Services;
