106858869a6e1f5b86104e1f7cff40a322b808c2
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-11 19:38:54 -0500

    Added deploy script on github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,10 @@
 
 module.exports = {
   siteMetadata: {
-    title: "WebDev Portfolio",
-    description: "This is WebDev Portfolio Site",
-    author: "@webdev",
-    twitterUsername: "@john_smilga",
+    title: "FullStack Portfolio",
+    description: "This is FullStack Portfolio Site",
+    author: "@FullStack",
+    twitterUsername: "@sebaslherrera",
     image: "/twitter-img.png",
     siteUrl: "https://testing-strapi-gatsby-build.netlify.app",
   },
