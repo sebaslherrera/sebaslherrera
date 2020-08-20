d1ce0666f0e6cd9b4960f9c098c3e69d61b132b0
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-20 18:13:53 -0500

    Added a Hero image TODO: improve the navbar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,45 +1,63 @@
-import React from "react";
-import Image from "gatsby-image";
-import { Link } from "gatsby";
-import { graphql, useStaticQuery } from "gatsby";
-import SocialLinks from "../constants/socialLinks";
+import React from "react"
+import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import SocialLinks from "../constants/socialLinks"
+import BackgroundImage from "gatsby-background-image"
+import styled from "styled-components"
 
 const query = graphql`
   {
-    file(relativePath: { eq: "hero-img.png" }) {
+    hero: file(relativePath: { eq: "nomadic-julien.jpg" }) {
       childImageSharp {
-        fluid {
+        fluid(quality: 90, maxWidth: 1920) {
           ...GatsbyImageSharpFluid
         }
       }
     }
   }
-`;
+`
 
-const Hero = () => {
-  const {
-    file: {
-      childImageSharp: { fluid },
-    },
-  } = useStaticQuery(query);
+const Hero = ({ className }) => {
+  const { hero } = useStaticQuery(query)
+
+  const backgroundFluidImageStack = [
+    hero.childImageSharp.fluid,
+    `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3))`,
+  ].reverse()
+
   return (
-    <header className="hero">
-      <div className="section-center hero-center">
-        <article className="hero-info">
-          <div>
-            <div className="underline"></div>
-            <h1>I'm Sebastian</h1>
-            <h4>Backend engineer</h4>
-            <Link to="/contact" className="btn">
-              Contact me!
-            </Link>
-            <SocialLinks />
-          </div>
-        </article>
-        <Image fluid={fluid} className="hero-img" />
-      </div>
+    <header>
+      <BackgroundImage
+        fluid={backgroundFluidImageStack}
+        Tag="section"
+        className={className}
+      >
+        <div className="section-center hero-center">
+          <article className="hero-info">
+            <div>
+              <div className="underline"></div>
+              <h1>I'm Sebastian</h1>
+              <h4>Backend engineer</h4>
+              <Link to="/contact" className="btn">
+                Contact me!
+              </Link>
+              <SocialLinks />
+            </div>
+          </article>
+        </div>
+      </BackgroundImage>
     </header>
-  );
-};
+  )
+}
 
-export default Hero;
+const StyledHeroSection = styled(Hero)`
+  height: 100vh;
+  width: 100%;
+  background-position: bottom center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-top: -5rem;
+  padding-top: 5rem;
+`
+
+export default StyledHeroSection
